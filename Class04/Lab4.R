7f3ab05b4358ae7d5b64b626b4095cf68d3689a0
cac007@ucsd.edu <carlychang@Carlys-MacBook-Air.local>
2025-02-25 11:36:28 -0800

    added classes

---
+++ Class04/Lab4.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Class 4: Intro to R"
+#' author: "Carly Chang (PID: A16843962)"
+#' ---
+
+
+x <- 1:50
+plot(x,sin(x))
+
+#make this nicer
+plot(x,sin(x),col="blue",type="l",lwd=3)
